2f66b15bfe3f1ba39da9ecb56ab77ea870176fc0
Jia <564252806@qq.com>
2018-08-09 22:25:13 +0800

    登录页面完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/Login';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-
+    {name: 'Login', path: '/login', component: Login}
   ]
 });
